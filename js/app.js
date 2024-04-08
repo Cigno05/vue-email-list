057a00e9db1c7ad78a50a66757bd4b8f384af26a
Cigno05 <paolocalcagni.82@gmail.com>
2024-04-08 18:24:08 +0200

    ADD v-for in HTML

--- js/app.js
+++ js/app.js
@@ -7,33 +7,36 @@ createApp({
   data() {
     return {
       arrayMails: [],
-      arrayMailsComplete: []
+      // arrayMailsComplete: [],
+      mailsLength: 30,
+
     }
   },
   methods: {
-    
-    fetchArraymails(n) {
-      for (let i = 0; i < n; i++) {
-        
-        axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((res) => {
-          this.arrayMails.push(res.data.response);
-          if (this.arrayMails.length === n) {
-            this.arrayMailsComplete = this.arrayMails
 
-            console.log(this.arrayMails.length)
-            console.log(this.arrayMailsComplete)
-          }
-        })
-        
+    fetchArraymails() {
+
+      for (let i = 0; i < this.mailsLength; i++) {
+
+        axios
+          .get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((res) => {
+            this.arrayMails.push(res.data.response);
+            // if (this.arrayMails.length === n) {
+            //     this.arrayMailsComplete = this.arrayMails
+
+            //     console.log(this.arrayMails.length)
+            //     console.log(this.arrayMailsComplete)
+            //   }
+          })
+
       }
     }
   },
   created() {
-      this.fetchArraymails(10)
-},
-  
+    this.fetchArraymails(10)
+  },
+
 }).mount('#app')
 
 
