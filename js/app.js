45040d34d9c921a506f1925fa5b0f82dc9cf4655
Cigno05 <paolocalcagni.82@gmail.com>
2024-04-08 16:02:18 +0200

    ADD bonus

--- js/app.js
+++ js/app.js
@@ -6,29 +6,34 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
-      number: null,
-      arrayMails: []
-
+      arrayMails: [],
+      arrayMailsComplete: []
     }
-  },
-  created() {
-    
-    this.fetchArraymails(10)
   },
   methods: {
     
     fetchArraymails(n) {
       for (let i = 0; i < n; i++) {
+        
         axios
         .get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((res) => {
           this.arrayMails.push(res.data.response);
+          if (this.arrayMails.length === n) {
+            this.arrayMailsComplete = this.arrayMails
+
+            console.log(this.arrayMails.length)
+            console.log(this.arrayMailsComplete)
+          }
         })
+        
       }
     }
-  }
-
+  },
+  created() {
+      this.fetchArraymails(10)
+},
+  
 }).mount('#app')
 
 
